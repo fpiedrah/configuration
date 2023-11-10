5a07ad6f083be7dcbc036b7986947018c0b24ad1
Francisco J. Piedrahita-Velez <fpiedrah@brown.edu>
2023-11-09 21:24:05 -0500

    Improved kitty config

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+# Remove greeting
+set -g fish_greeting
+
+# Aliases
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
+
+alias ls="lsd"
+alias tree="ls --tree"
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/opt/homebrew/opt/micromamba/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/Users/fpiedrah/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
+
+# Activate base env
+micromamba activate
