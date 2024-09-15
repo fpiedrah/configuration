d09a76b574f532703f527d81c84987b006b2972d
Francisco Piedrahita Velez <fpiedrah@macbook-air.rlab.cs.brown.edu>
2024-09-15 15:40:34 -0400

    Make configurations compatible with stow

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
-# Remove greeting
+# Remove Greeting
 set -g fish_greeting
+
+# Add Brew To Path
+fish_add_path /opt/homebrew/bin
 
 # Aliases
 alias v="nvim"
@@ -9,12 +12,9 @@ alias vim="nvim"
 alias ls="lsd"
 alias tree="ls --tree"
 
-# Nextflow
-fish_add_path $HOME/.nextflow
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /Users/fpiedrah/.anaconda/bin/conda
-    eval /Users/fpiedrah/.anaconda/bin/conda "shell.fish" "hook" $argv | source
+  eval /Users/fpiedrah/.anaconda/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
